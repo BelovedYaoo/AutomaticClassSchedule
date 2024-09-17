47ff6f9d69195d4c25fb6cc5b2142a3ed329a4a0
BelovedYaoo <belovedyaoo@aliyun.com>
2024-09-17 19:39:19 +0800

    新增(Java): Java后端初始化
    从OpenIAM迁移

---
+++ Java/src/main/java/top/belovedyaoo/acs/ACSApplication.java
@@ -0,0 +1,19 @@
+package top.belovedyaoo.acs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目启动类
+ *
+ * @author BelovedYaoo
+ * @version 1.0
+ */
+@SpringBootApplication
+public class ACSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ACSApplication.class, args);
+    }
+
+}
